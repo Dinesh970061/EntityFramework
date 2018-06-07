7b848ddfd5cd2b4204ca02d0eb7ae52132cce261
dinesh.nallagachu <email>
2018-06-08 00:34:20 +0530

    Added EF

---
+++ EntityStudent/Data/ContextStudent.cs
@@ -0,0 +1,19 @@
+﻿using EntityStudent.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityStudent.Data
+{
+    public class ContextStudent : DbContext
+    {
+        public DbSet<ModelStudent> TableStudent { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DNALLAGACHU01; Database=StudentData; Trusted_Connection=True; MultipleActiveResultSets=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
